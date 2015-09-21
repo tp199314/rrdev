d3cc2d0318c8fef2d5d500f278e156da4be94b4c
Tobias <tobias.prein@googlemail.com>
2015-09-21 21:27:44 +0200

    tweaks on landing page

--- index.php
+++ index.php
@@ -1,25 +1,18 @@
 <?php get_header(); ?>
       <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
+          <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
           <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
               <div class="panel panel-success">
-                 <div class="panel-heading">Panels heading</div>
-                <div class="panel-body">
-                  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-      <div class="entry">
-         <?php the_content(); ?>
-      </div>
-   <?php endwhile; ?>
-
-      <p align="center"><?php next_posts_link('&laquo; &Auml;ltere Eintr&auml;ge') ?> | <?php previous_posts_link('Neuere Eintr&auml;ge &raquo;') ?></p>
-
-   <?php endif; ?>
-                </div>
+                <div class="panel-heading"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></div>
+                <div class="panel-body"><?php the_content(); ?></div>
               </div>
             </div>
           </div>
+          <?php endwhile; ?>
+        <p align="center"><?php next_posts_link('&laquo; &Auml;ltere Eintr&auml;ge') ?> | <?php previous_posts_link('Neuere Eintr&auml;ge &raquo;') ?></p>
+      <?php endif; ?>
         </div>
         <?php get_sidebar(); ?>
       </div>
